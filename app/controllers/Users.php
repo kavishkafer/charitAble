6ef92766cfd4d499525082550f38fdb23299be06
Amasha Wijesinghe <amashawijesinghe1999@gmail.com>
2023-01-09 11:38:44 +0530

    ok

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -77,7 +77,7 @@ class Users extends Controller{
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
                 //Register User
                 
-                if($this->userModel->register($data)){
+                if($this->userModel->register_ben($data)){
                     flash('register_success', 'You are registered and can log in');
                     $email = new Email($data['email']);
                     $email->sendVerificationEmail($data['email'], $otp_code);
@@ -134,7 +134,7 @@ class Users extends Controller{
                 'password_err' =>''    
               ];
                // Check for user/email
-               if($this->userModel->findUserByEmail($data['email'])){
+               if($this->userModel->findUserByEmail_ben($data['email'])){
                 // User found
               } else {
                 // User not found
@@ -154,10 +154,10 @@ class Users extends Controller{
               if(empty($data['email_err']) && empty($data['password_err'])){
                 // Validated
                 // Check and set logged in user
-                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                $loggedInUser = $this->userModel->signin_ben($data['email'], $data['password']);
                 if($loggedInUser){
                   // Create Session
-                  $this->createUserSession($loggedInUser);
+                  $this->createUserSession_ben($loggedInUser);
                   
                 
                 } else {
@@ -184,13 +184,13 @@ class Users extends Controller{
               $this->view('users/login_ben', $data);
             }
             }
-            public function createUserSession($user){
+            public function createUserSession_ben($user){
                 $_SESSION['user_id'] = $user->B_Id;
                 $_SESSION['user_email'] = $user->B_Email;
                 $_SESSION['user_name'] = $user->B_Name;
                 redirect('request_bens');
               }
-              public function logout(){
+              public function logout_ben(){
                 unset($_SESSION['user_id']);
                 unset($_SESSION['user_email']);
                 unset($_SESSION['user_name']);
@@ -244,6 +244,203 @@ class Users extends Controller{
                 }
                 $this->view('users/signup_verification', $data);
             }
+
+//Donor
+
+public function signup_dons(){
+  // Check for POST
+  
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      // Process form
+
+      //sanitize POST data
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+   
+      // Init data
+      $data = [
+          'name' => trim($_POST['name']),
+          'email' => trim($_POST['email']),  
+          'tel_no' => trim($_POST['tel_no']),
+          'address' => trim($_POST['address']),
+          'password' => trim($_POST['password']),
+          'confirm_password' => trim($_POST['confirm_password']),
+          'name_err' => '',
+          'email_err' => '',
+          'tel_no_err' => '',
+          'address_err' => '',
+          'password_err' => '',
+          'confirm_password_err' => ''
+      ];
+      //Validate Email
+      if(empty($data['email'])){
+          $data['email_err'] = 'Please enter email';
+      }else{
+        //check email
+        if($this->userModel->findUserByEmail_don($data['email'])){
+            $data['email_err'] = 'Email is already taken';  
+        }
+    }
+
+      //Validate Name
+      if(empty($data['name'])){
+          $data['name_err'] = 'Please enter name';
+      }
+      //Validate Telephone Number
+      if(empty($data['tel_no'])){
+          $data['tel_no_err'] = 'Please enter telephone number';
+      }
+      //Validate Address
+      if(empty($data['address'])){
+          $data['address_err'] = 'Please enter address';
+      }
+      //Validate Password
+      if(empty($data['password'])){
+          $data['password_err'] = 'Please enter password';
+      } elseif(strlen($data['password']) < 6){
+          $data['password_err'] = 'Password must be at least 6 characters';
+      }
+      //Validate Confirm Password
+      if(empty($data['confirm_password'])){
+          $data['confirm_password_err'] = 'Please confirm password';
+      } else {
+          if($data['password'] != $data['confirm_password']){
+              $data['confirm_password_err'] = 'Passwords do not match';
+          }
+      }
+      // Make sure errors are empty
+      if(empty($data['email_err']) && empty($data['name_err']) && empty($data['tel_no_err']) && empty($data['address_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
+          // Validated
+          //Hash
+          $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+          //Register User
+          if($this->userModel->signup_don($data)){
+              flash('register_success', 'You are registered and can log in');
+              redirect('users/login_dons');
+          } else {
+              die('Something went wrong');
+          }
+
+          
+      } else {
+          // Load view with errors
+          $this->view('users/signup_dons', $data);
+      }
+      
+
+      // Load view
+      $this->view('users/signup_dons', $data);
+  }
+  else{
+      // Init data
+      $data = [
+          'name' => '',
+          'email' => '',  
+          'tel_no' => '',
+          'address' => '',
+          'password' => '',
+          'confirm_password' => '',
+          'name_err' => '',
+          'email_err' => '',
+          'tel_no_err' => '',
+          'address_err' => '',
+          'password_err' => '',
+          'confirm_password_err' => ''
+      ];
+      // Load view
+      $this->view('users/signup_dons', $data);
+  }
+}
+
+
+public function login_dons(){
+  // Check for POST
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      // Process form
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+  
+      // Init data
+      $data =[
+          'email' => trim($_POST['email']),
+          'password' => trim($_POST['password']),
+          'email_err' => '',
+          'password_err' =>''    
+        ];
+
+         // Check for user/email
+         if($this->userModel->findUserByEmail_don($data['email'])){
+          // User found
+        } else {
+          // User not found
+          $data['email_err'] = 'No user found';
+        }
+
+
+        // Validate Email
+        if(empty($data['email'])){
+          $data['email_err'] = 'Please enter email';
+        }
+
+        // Validate Password
+        if(empty($data['password'])){
+          $data['password_err'] = 'Please enter password';
+        }
+       
+        // Make sure errors are empty
+        if(empty($data['email_err']) && empty($data['password_err'])){
+          // Validated
+          // Check and set logged in user
+          $loggedInUser = $this->userModel->login_don($data['email'], $data['password']);
+          if($loggedInUser){
+            // Create Session
+            $this->createUserSession_don($loggedInUser);
+            
+          
+          } else {
+            $data['password_err'] = 'Password incorrect';
+
+            $this->view('users/login_dons', $data);
+          }
+        } else {
+          // Load view with errors
+          $this->view('users/login_dons', $data);
+        }
+
+
+      } else {
+        // Init data
+        $data =[    
+          'email' => '',
+          'password' => '',
+          'email_err' => '',
+          'password_err' => '',        
+        ];
+
+        // Load view
+        $this->view('users/login_dons', $data);
+      }
+      }
+      public function createUserSession_don($user){
+        $_SESSION['user_id'] = $user->D_Id;
+        $_SESSION['user_email'] = $user->D_Email;
+        $_SESSION['user_name'] = $user->D_Name;
+        redirect('pages/about');
+      }
+      public function logout_don(){
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('users/login_dons/0');
+      }
+      
+      /*public function isLoggedIn(){
+        if(isset($_SESSION['user_id'])){
+          return true;
+        } else {
+          return false;
+        }
+      }*/
+
           }
       
         
--- app/models/User.php
+++ app/models/User.php
@@ -4,7 +4,9 @@ class User {
     public function __construct(){
         $this->db = new Database;
     }
-    public function findUserByEmail($email){
+
+    //Beneficiary
+    public function findUserByEmail_ben($email){
         $this->db->query('SELECT * FROM beneficiary_details WHERE B_Email = :email');
         $this->db->bind(':email', $email);
         $row = $this->db->single();
@@ -15,7 +17,7 @@ class User {
             return false;
         }
     }
-    public function register($data){
+    public function register_ben($data){
         $this->db->query('INSERT INTO beneficiary_details (B_Name,B_Email,B_Tpno,B_Address,B_Password,otp) VALUES(:name, :email,:telephone_number,:address, :password,:otp)');
 
         ;
@@ -33,7 +35,7 @@ class User {
             return false;
         }
     }
-    public function login($email, $password){
+    public function signin_ben($email, $password){
         $this->db->query('SELECT * FROM beneficiary_details WHERE B_Email = :email');
         $this->db->bind(':email', $email);
         $row = $this->db->single();
@@ -44,11 +46,62 @@ class User {
             return false;
         }
     }
-    public function getUserById($id){
+    public function getUserById_ben($id){
         $this->db->query('SELECT * FROM beneficiary_details WHERE B_Id = :B_id');
         $this->db->bind(':B_id', $id);
         $row = $this->db->single();
         return $row;
+    }
+
+    //Donor
+    //Register user
+    public function signup_don($data){
+        $this->db->query('INSERT INTO donor_details (D_Name, D_Email, D_Tel_no, D_Address, D_password) VALUES(:name, :email, :tel_no, :address, :password)');
+        //Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':tel_no', $data['tel_no']);
+        $this->db->bind(':address', $data['address']);
+        $this->db->bind(':password', $data['password']);
+
+        //Execute
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Login user(find user by email)
+    public function login_don($email, $password){
+        $this->db->query('SELECT * FROM donor_details WHERE D_Email = :email');
+        //Bind values
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+
+        $hashed_password = $row->D_Password;
+        if(password_verify($password, $hashed_password)){
+            return $row;
+        }else{
+            return false;
+        }
+    }
+
+    //find user by email
+    public function findUserByEmail_don($email){
+        $this->db->query('SELECT * FROM donor_details WHERE D_Email = :email');
+        //Bind values
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+
+        //check row
+        if($this->db->rowCount() > 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
