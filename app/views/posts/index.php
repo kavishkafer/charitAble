94cb6d37d3b9cc3e19cc591885d8308d52c54d95
IshiniAvindya <61535113+IshiniAvindya@users.noreply.github.com>
2023-02-01 14:54:37 +0530

    Update index.php
    updated index.php of post view

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,24 +1,39 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/style.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/post/post_view.css">
 <body>
 
 
 <div class="row">
-<div class="col">
-    <h1>Posts</h1>
+<div class="col-head">
+    
+<div class="header">
+  <a href="#default" class="logo"><img src="img/img_dons/logow.svg" alt="logo"></a>
+  <div class="header-right">
+    <a href="#about">Hi James!</a>
+  </div>
+
 </div>
+</div>
+
+<br> <br> 
+
 <div class="col">
     <a href="<?php echo URLROOT; ?>/posts/add" class="addPostLink">
     <button class="addPost">Add Post</button>
     </a>
 </div>
 </div>
+<br>
 
 <?php foreach($data['posts'] as $post) : ?>
     <div class="card">
-            <h4 class="title"><?php echo $post->title; ?></h4>
-             
+            <h2 class="title"><?php echo $post->title; ?></h2>
+            <!-- the following div is for image showing. please note that this is a dummy code-->
+            <div class="post-img-div">
+                <?php echo $post->image; ?>
+             </div> 
+
                 <div>
                 <p>Written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?> </p>
                 </div>
