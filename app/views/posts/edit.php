5566faddd6e7ae08889ad9989cb602d4134f818a
IshiniAvindya <61535113+IshiniAvindya@users.noreply.github.com>
2023-02-01 14:53:35 +0530

    Update edit.php
    Updated edit.php in posts view

--- app/views/posts/edit.php
+++ app/views/posts/edit.php
@@ -1,10 +1,25 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/post/post_edit.css">
 
+<div class="row">
+<div class="col-head">
+    
+<div class="header">
+  <a href="#default" class="logo"><img src="img/img_dons/logo.svg" alt="logo"></a>
+  <div class="header-right">
+    <a href="#about">Hi James!</a>
+  </div>
 
-<a href="<?php echo URLROOT; ?>/posts" class="btm">Back</a>
+</div>
+</div>
+<br> <br> 
+<a href="<?php echo URLROOT; ?>/posts" class="btn">Back</a>
+
+<br> <br>
 <div>
-<h2>Edit post</h2>
+<h1>Edit post</h1>
+
+<div class="card">
 <form action="<?php echo URLROOT; ?>/posts/edit/<?php echo $data['$id']; ?>" method="post">
 <div class="div">
         <label for="title">Title</label>
@@ -19,7 +34,16 @@
         <div class=warn><?php echo $data['body_err']; ?></div>
          
 </div>
+
+<div class="div">
+        <label for="image">Image</label>
+        <input name="image" type="file" class="addP <?php echo (!empty($data['image_err'])) ?>" value="<?php echo $data['image']; ?>">
+        <div class=warn><?php echo $data['image_err']; ?></div>
+         
+</div>
+
 <input type="submit" value="Submit" class="addPbtn">
 </form>
+</div>
 </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
