e74c2f3c7349673ab5e3109618d002bea5cd9a66
IshiniAvindya <ishiniavindya2000@gmail.com>
2023-02-18 19:50:11 +0530

    comments
    comments

--- app/controllers/Comments.php
+++ app/controllers/Comments.php
@@ -2,6 +2,8 @@
 class Comments extends Controller{
 public function __construct() {
     $this->commentModel = $this-> model('Comment');
+    $this->userModel = $this->model('User');
+
 }
 
 
@@ -20,10 +22,50 @@ public function __construct() {
         ];
 
         if($this->commentModel->addComment($data)){
-            echo ('you comment added');
+            flash('comment_msg','your comment added');
         }
         else {
             die('something went wrong');
+        }
+    }
+
+    public function showComments($id){
+    $comments = $this->commentModel->getComments($id);
+
+    //render html thread
+        foreach ($comments as $comment) {
+           echo '<div class="comment-container">';
+
+            echo '<div class="comment-left">';
+                echo '<img src="<?php echo URLROOT;?>/img/comments/avatar.png" alt="">';
+            echo '</div>';
+            echo '<div class="comment-right">';
+                echo '<div class="comment-right-header">';
+                    echo '<div class="comment-user-name">';
+                        echo $comment->user_id;
+            echo '</div>';
+                    echo '<div class="comment-posted-at">';
+            echo convertTimeReadableFormat($comment->created_at);
+                    echo '</div>';
+                echo '</div>';
+                echo '<div class="comment-right-body">';
+               echo $comment->content;
+   echo '</div>';
+                echo '<div class="comment-right-footer">';
+
+                    echo '<div class="comment-likes">';
+                        echo '<img src="<?php echo URLROOT;?>/img/comments/like-btn.png" alt="">';
+                       echo '<div class="comment-likes-count">0</div>';
+                    echo '</div>';
+
+            echo '<div class="comment-dislikes">';
+                        echo '<img src="<?php echo URLROOT;?>/img/comments/dislike-btn.png" alt="">';
+                        echo '<div class="comment-likes-count">0</div>';
+                   echo '</div>';
+                echo '</div>';
+            echo '</div>';
+        echo '</div>';
+        echo '<br>';
         }
     }
 }
--- app/models/Comment.php
+++ app/models/Comment.php
@@ -1,25 +1,35 @@
 <?php
-class Comment{
+class Comment
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database();
     }
 
-    public function addComment($data){
+    public function addComment($data)
+    {
         $this->db->query('INSERT INTO comments (post_id, user_id, content) VALUES(:post_id, :user_id, :content)');
-      // Bind values
-      $this->db->bind(':post_id', $data['post_id']);
-      $this->db->bind(':user_id', $data['user_id']);
-      $this->db->bind(':content', $data['content']);
+        // Bind values
+        $this->db->bind(':post_id', $data['post_id']);
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':content', $data['content']);
 
-      // Execute
-      if($this->db->execute()){
-        return true;
-      } else {
-        return false;
-      }
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    public function getComments($id)
+    {
+        $this->db->query('SELECT * FROM comments INNER JOIN registered_users ON comments.user_id = registered_users.User_Id WHERE post_id = :post_id ORDER BY comments.created_at DESC');
+        $this->db->bind(':post_id', $id);
+        return $this->db->resultSet();
     }
+}
 
 
